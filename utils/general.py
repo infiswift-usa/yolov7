acfb484e8382cfb04ef15839fc0b1ef248e42942
Narayanan Nagarajan <41759669+narayanan-infiswift@users.noreply.github.com>
2025-02-17 16:25:52 -0800

    Fix NMS to operate on CPU tensors in non_max_suppression function

--- utils/general.py
+++ utils/general.py
@@ -681,7 +681,7 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
         # Batched NMS
         c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
         boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        i = torchvision.ops.nms(boxes.cpu(), scores.cpu(), iou_thres)  # NMS on CPU
         if i.shape[0] > max_det:  # limit detections
             i = i[:max_det]
         if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
